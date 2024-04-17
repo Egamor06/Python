a259e08e278429e4c315e921c40b6458bd03471b
Egamor <egor.morozov10@icloud.com>
2024-04-17 15:30:40 +0300

    Решение задач

---
+++ 1. Введение/Задачи/Расстояние между точками.py
@@ -0,0 +1,6 @@
+import math
+x1 = int(input('x1: '))
+x2 = int(input('x2: '))
+y1 = int(input('y1: '))
+y2 = int(input('y2: '))
+print(f"Расстояние между точками={math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)}")
